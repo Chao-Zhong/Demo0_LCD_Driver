eb2630247b44c8ba72042bcb160ce28465f08099
ZhuanZ1 <1801372279@qq.com>
2025-10-18 02:16:32 +0800

    添加了WIFI扫描功能，能够扫描附近的wifi并在串口终端上显示信息，下一步通过FreeRTOS队列实现热点信息的传递，设计接口在LCD上显示

---
+++ dev_driver/wifi/wifi.h
@@ -0,0 +1,6 @@
+#ifndef _WIFI_H
+#define _WIFI_H
+
+void start_task_wifi_scan(void);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include "lcd_driver.h"
+#include "wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/FreeRTOSConfig.h"
 #include "freertos/task.h"
@@ -6,15 +7,15 @@
 void lcd(void *arg);
 
 void app_main(){
-    xTaskCreate(lcd, "lcd", 4068, NULL, 5, NULL);
+    xTaskCreate(lcd, "lcd", 4096, NULL, 5, NULL);
 }
 
 void lcd(void *arg){
     lcd_init();
-    esp_lcd_panel_swap_xy(panel_handle, false);
     lcd_panel_set_backgrund(panel_handle, BLACK);
     lcd_panel_draw_char(50, 50, 'A', WHITE);
     lcd_panel_draw_string(0, 160, "Hello ESP32", WHITE);
+    start_task_wifi_scan();
     while(1){
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
